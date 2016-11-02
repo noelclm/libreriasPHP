963aaeb243307ac9b7865705e96ff8605e605472
Noel <noelclm@gmail.com>
2016-11-02 10:30:31 +0100

    Update WooCommerce_Curl.php

--- WooCommerce_Curl.php
+++ WooCommerce_Curl.php
@@ -9,15 +9,36 @@
  * @description Funciones para conectarse a un WooCommerce mediante CURL
  */
 
+/*
+// Ejemplos
+
+$categorias = obtenerWooCommerce("products/categories");
+
+productos = obtenerWooCommerce("products");
+
+$data = array( "product_category" => array("name"=>$nombre));
+actualizarWooCommerce("products/categories",$id,$datos);
+
+borrarWooCommerce("products/categories",$id);
+
+$data = array( "product" => array( 'title' => "Titulo", 'description' => "Descripcion", 'sku' => Referencia , 'categories' => array( 'id' => $idCategoria)));
+createWooCommerce("products",$data);
+
+*/
+
 // Datos para conectarse al servidor
-define("URL", ""); // URL donde esta la pagina de WooCommerce
-define("CK", ""); // CONSUMER_KEY
-define("CS", ""); // CONSUMER_SECRET
+define("URL", "https://www.tupagina.com"); // URL donde esta la pagina de WooCommerce
+define("CK", "CONSUMER_KEY");
+define("CS", "CONSUMER_SECRET");
 define("SSL_VERIFYPEER", false);
 define("SSL_VERIFYHOST", false);
 define("RETURNTRANSFER", true);
 
-// Obtiene los datos de la ruta del WooCommerce que indique (Ej.: 'products', 'products/orders')
+/**
+ * @param string $ruta Ruta del WooCommerce
+ * @return object Objeto con la lista de los datos solicitados
+ * @description Obtiene los datos de la ruta del WooCommerce que indique
+ */
 function obtenerWooCommerce($ruta){
 
     $curl = curl_init();
@@ -34,14 +55,23 @@ function obtenerWooCommerce($ruta){
             print_r("Error: " . curl_error($curl)); 
     
     curl_close($curl); 
-
+    
+    $resultados = json_decode($resultados);
+        
     return $resultados;
     
 } // obtenerWooCommerce($ruta)
 
-// Crea una nueva entrada en la ruta que le indiques con los datos pasados
+/**
+ * @param string $ruta Ruta del WooCommerce
+ * @param array $datos Datos a insertar
+ * @return object El objeto creado
+ * @description Crea una nueva entrada en la ruta que le indiques con los datos pasados
+ */
 function crearWooCommerce($ruta,$datos){
 
+    $datos = json_encode($datos);
+    
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, SSL_VERIFYPEER);
     curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, SSL_VERIFYHOST);
@@ -59,11 +89,18 @@ function crearWooCommerce($ruta,$datos){
     
     curl_close($curl); 
 
+    $resultados = json_decode($resultados);
+    
     return $resultados;
     
 } // crearWooCommerce($ruta,$datos)
 
-// Borra la entrada en la ruta indicada 
+/**
+ * @param string $ruta Ruta del WooCommerce
+ * @param int $id ID del dato a borrar
+ * @return object El objeto eliminado
+ * @description Borra la entrada en la ruta indicada 
+ */
 function borrarWooCommerce($ruta,$id){
 
     $curl = curl_init();
@@ -81,13 +118,23 @@ function borrarWooCommerce($ruta,$id){
     
     curl_close($curl); 
 
+    $resultados = json_decode($resultados);
+    
     return $resultados;
     
 } // borrarWooCommerce($ruta,$id)
 
-// Actualiza la entrada en la ruta indicada
+/**
+ * @param string $ruta Ruta del WooCommerce
+ * @param int $id ID del dato a actualizar
+ * @param array $datos Datos a modificar
+ * @return object El objeto modificado
+ * @description Actualiza la entrada en la ruta indicada
+ */
 function actualizarWooCommerce($ruta,$id,$datos){
 
+    $datos = json_encode($datos);
+    
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, SSL_VERIFYPEER);
     curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, SSL_VERIFYHOST);
@@ -105,6 +152,8 @@ function actualizarWooCommerce($ruta,$id,$datos){
     
     curl_close($curl); 
 
+    $resultados = json_decode($resultados);
+    
     return $resultados;
     
 } // actualizarWooCommerce($ruta,$id,$datos)
