8a65b4db9d445dcf38ba1c601fb3cf84c6b04e9a
Noel <noelclm@gmail.com>
2017-09-05 21:26:06 +0200

    login v2

--- Login.php
+++ Login.php
@@ -3,7 +3,7 @@
 /**
  * Comprobacion de login
  *
- * @version 1.0
+ * @version 2.0
  * @author noelclm (https://github.com/noelclm)
  * @url https://github.com/noelclm/libreriasPHP/Login.php
  */
@@ -12,8 +12,8 @@
 // Definiciones (Opciones)
 //-------------------------------------------------------------------
 
-define("VARIAS_SESIONES", true); // Deja o no que puedas estar logueado en varios sitios
-define("TIEMPO_LOGIN", 604800); // En segundos (Un dia = 86400, Una semana = 604800)
+define("IDLE_TIME", 604800); // Seconds (1 dia = 86400, 1 semana = 604800, infinito = 0)
+define("MULTIPLE_SESSIONS", false);
 
 /*
 
@@ -22,40 +22,39 @@ Contraseñas tienen que estar guardadas con password_hash()
 // Tablas necesaria
 
 # -------------------------------------------
-# Estructura de `sesion`
+# Estructura de `session`
 # -------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `sesion` (
-    `idsesion`          INT(11) NOT NULL AUTO_INCREMENT,
-    `idusuario`         INT(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `session` (
+    `session_id`        INT(11) NOT NULL AUTO_INCREMENT,
+    `user_id`           INT(11) NOT NULL,
     `key`               VARCHAR(32) NOT NULL,
-    `activodesde`       INT(11) NOT NULL,
-    `ultimaactividad`   INT(11) NOT NULL,
+    `last_active`       INT(11) NOT NULL,
     `ip`                VARCHAR(15) NOT NULL,
-    PRIMARY KEY  (`idsesion`)
+    PRIMARY KEY  (`session_id`)
 )ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci AUTO_INCREMENT = 1;
 
 # -------------------------------------------
-# Estructura de `usuario`
+# Estructura de `user`
 # -------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `usuario` (
-    `IDUsuario`         INT(11) NOT NULL AUTO_INCREMENT,
-    `nombre`            VARCHAR(75) NOT NULL,
-    `usuario`           VARCHAR(75) NOT NULL,
-    `clave`             VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user` (
+    `user_id`           INT(11) NOT NULL AUTO_INCREMENT,
+    `name`              VARCHAR(75) NOT NULL,
+    `user`              VARCHAR(75) NOT NULL,
+    `password`          VARCHAR(255) NOT NULL,
     `email`             VARCHAR(255) NOT NULL,
-    `rol`               INT(11) NOT NULL,
-    PRIMARY KEY  (`IDUsuario`)
+    `role`              INT(11) NOT NULL,
+    PRIMARY KEY  (`user_id`)
 )ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci AUTO_INCREMENT = 1;
 
 
 Ejemplo del formulario login
 
-<form action="index.php" method="post">
-    <label for="Usuario">Nombre:</label><input type="text" name="Usuario" required ><br><br>
-    <label for="clave">Contraseña:</label><input type="password" name="clave" required ><br><br>
-    <label for="guardarSesion">Guardar</label><input type="checkbox" value="true" name="guardarSesion" ><br><br>
+<form action="" method="post">
+    <label for="user">Nombre:</label><input type="text" name="user" required ><br><br>
+    <label for="password">Contraseña:</label><input type="password" name="password" required ><br><br>
+    <label for="save">Guardar</label><input type="checkbox" value="true" name="save" ><br><br>
     <input type="hidden" name="login" value=true>
     <button type="submit">Entrar</button>
 </form>
@@ -63,7 +62,9 @@ Ejemplo del formulario login
 Ejemplo de uso
 
 $login = new Login();
-$codigo = $login->comprobarLogin();
+$codigo = $login->check();
+
+Las contraseñas tienen que estar guardadas con password_hash()
 
 */
 
@@ -82,7 +83,7 @@ class Login {
         session_start();
         session_regenerate_id(true);
 
-    } // function __construct
+    } 
 
     /**
      * Hace todas las comprobaciones al cargar la pagina
@@ -96,213 +97,224 @@ class Login {
      *
      * @return int Codigo mensaje 
      */
-    function comprobarLogin (){
+    public function check (){
 
-        if(!isset($_SESSION['Login'])){ $_SESSION['Login'] = false; }
+        if(!isset($_SESSION['login'])){ 
+            $_SESSION['login'] = false; 
+            return 1005;
+        }
 
         // Borra las sesiones que han caducado
-        borrarSesionesAntiguas();
+        $this->deleteOldSession();
 
         // Si ha pulsado en logout
         if(((isset($_GET['logOut']) && $_GET['logOut'] === true) || (isset($_POST['logOut']) && $_POST['logOut'] === true)) && (isset($_SESSION['Login']) && $_SESSION['Login'] === true)){
-            
-            unset($_POST);
-            unset($_GET);
-            logOut();
+            $this->logOut();
             return 1003;
-
         }
 
         // Si no esta logeado comprueba las cookies
-        if(!$_SESSION['Login'])
-            comprobarCookie();
-
-        // Si esta logueado mira si ha expirado la sesion
-        if($_SESSION['Login']){
-            if(!comprobarSesion()){
-                return 1004;
-            }
-        }else{
-            return 1005;
+        if (!$_SESSION['login']) { $this->checkCookie(); } 
+        else { // Si esta logueado mira si ha expirado la sesion
+            if(!$this->chekSesion()){ return 1004; } 
         }
         
-
         // Si se acaba de loguear
-        if(isset($_POST['login']) && $_POST['login'] === true && isset($_POST['Usuario']) && isset($_POST['clave'])){
-
+        if(isset($_POST['login']) && $_POST['login'] == true && isset($_POST['user']) && isset($_POST['password'])){
+            
             // Si no esta marcado para que se guarde la sesion
-            if(!isset($_POST['guardarSesion'])){
-                $_POST['guardarSesion'] = false;
-            }
+            if(!isset($_POST['save'])){ $_POST['save'] = false; }
 
-            // Revisa si va codigo en los datos de entrada
-            $usuario = escaparCodigo($_POST['Usuario']);
-            $clave = escaparCodigo($_POST['clave']);
-
-            if(login($usuario,$clave,$_POST['guardarSesion'])){
-                unset($_POST);
-                return 1001;
-            }else{
-                unset($_POST);
-                return 1002;
-            }
+            if($this->login(escaparCodigo($_POST['user']),escaparCodigo($_POST['password']),$_POST['save'])){ return 1001; }
+            else{ return 1002; }
+            
+            unset($_POST);
 
         }
 
-        // Sigue logueado
         return 1000;
 
-    } // function comprobarLogin
+    } 
 
     /**
      * Borra de la base de datos las sesiones inactivas
      *
-     * @access protected
      */
-    protected function borrarSesionesAntiguas (){
+    private function deleteOldSession (){
         
-        if(isset($_SESSION['Key'])){
-            borrarSQL("DELETE FROM sesion WHERE ultimaactividad + ". TIEMPO_LOGIN ." < " . time() . ";"); 
+        if(IDLE_TIME > 0 && isset($_COOKIE["save"])){
+            $bd = new AccesoDatos();
+            $bd->ejecutar("DELETE FROM session WHERE last_active + ". IDLE_TIME ." < " . time() . ";");
+            $bd->cerrar();
+            unset($bd);
         }
 
-    } // function borrarSesionesAntiguas
+    } 
 
     /**
      * Comprueba las cookies con los datos de la base de datos
      *
-     * @access protected
      */
-    protected function comprobarCookie (){
+    private function checkCookie (){
 
-        if(isset($_COOKIE["IDUsuario"]) && isset($_COOKIE["Key"]) && isset($_COOKIE["Login"]) && isset($_COOKIE["DireccionIP"]) && isset($_COOKIE["Rol"])&& isset($_COOKIE["Usuario"]) && isset($_COOKIE["Nombre"])){
-           
-            $resultado = consultaSQL("SELECT * FROM `sesion` WHERE `ip` = '".$_COOKIE['DireccionIP']."' AND `IDUsuario` = ".$_COOKIE['IDUsuario']." AND `key` = '".$_COOKIE['Key']."';");
+        if(isset($_COOKIE["user_id"]) && isset($_COOKIE["key"]) && isset($_COOKIE["login"]) && isset($_COOKIE["ip"]) && isset($_COOKIE["user"]) && isset($_COOKIE["name"])){
+            
+            $bd = new AccesoDatos();
+            $execute = $bd->ejecutar("SELECT * FROM `session` WHERE `ip` = '".$_COOKIE['ip']."' AND `IDUsuario` = ".$_COOKIE['user_id']." AND `key` = '".$_COOKIE['key']."';");
+            
+            if ($execute) {
+                while(($row = $bd->nextRow()) !== false){
+                    $result[] = $row;
+                }
+            }
+            
+            if(count($result)){
 
-            if(count($resultado)){
+                $_SESSION['name'] = $_COOKIE['name'];
+                $_SESSION['user'] = $_COOKIE['user'];
+                $_SESSION['user_id'] = $_COOKIE['user_id'];
+                $_SESSION['key'] = $_COOKIE['key'];
+                $_SESSION['ip'] = $_COOKIE['ip'];
+                $_SESSION['last_activity'] = $_SERVER['REQUEST_TIME'];
+                $_SESSION['login'] = $_COOKIE['login'];
 
-                $_SESSION['Nombre'] = $_COOKIE['Nombre'];
-                $_SESSION['Usuario'] = $_COOKIE['Usuario'];
-                $_SESSION['IDUsuario'] = $_COOKIE['IDUsuario'];
-                $_SESSION['Key'] = $_COOKIE['Key'];
-                $_SESSION['DireccionIP'] = $_COOKIE['DireccionIP'];
-                $_SESSION['LastActivity'] = $_SERVER['REQUEST_TIME'];
-                $_SESSION['Login'] = $_COOKIE['Login'];
-                $_SESSION['Rol'] = $_COOKIE['Rol'];
+            }
+            
+            $bd->cerrar();
+            unset($bd);
 
-            } // if(count($resultado))
+        }
 
-        }  // if(isset($_COOKIE["IDUsuario"])...
-
-    } // function comprobarCookie
+    }
 
     /**
      * Comprueba los datos de la variable $_SESSION con los datos de la base de datos y guarda el ultimo acceso
      *
-     * @access protected
      * @return boolean True si el login es valido
      */
-    protected function comprobarSesion (){
+    private function chekSesion (){
 
-        $resultado = consultaSQL("SELECT * FROM `session` WHERE `key` = '".$_SESSION['Key']."';");
+        $bd = new AccesoDatos();
+        $execute = $bd->ejecutar("SELECT * FROM `session` WHERE `key` = '".$_SESSION['key']."';");
 
-        if(count($resultado)){
+        if ($execute) {
+            while(($row = $bd->nextRow()) !== false){
+                $result[] = $row;
+            }
+        }
 
-            $resultado = $resultado[0];
+        if(count($result)){
 
-            if($resultado['idusuario'] == $_SESSION['IDUsuario'] && $resultado['ip'] == $_SESSION['DireccionIP']){
+            $result = $result[0];
 
-                modificarSQL("UPDATE sesion SET `ultimaactividad` = ".$_SERVER['REQUEST_TIME']." WHERE `key` = '".$_SESSION['Key']."';");
+            if($result['user_id'] == $_SESSION['user_id'] && $result['ip'] == $_SESSION['ip']){
+
+                $bd->ejecutar("UPDATE session SET `last_active` = ".$_SERVER['REQUEST_TIME']." WHERE `key` = '".$_SESSION['key']."';");
+                $bd->cerrar();
+                unset($bd);
                 return true;
 
-            } // if($resultado['IDUsuario'] == $_SESSION['IDUsuario']...
+            }
             
-        } // if(count($resultado))
-
+        } 
+        
+        $bd->cerrar();
+        unset($bd);
         logOut();
         return false;
 
-    } // function comprobarSesion
+    } 
 
     /**
      * Comprueba si el usuario y contraseña introducidos son correctos
      *
-     * @access protected
-     * @param string $nombre Nombre del usuario
+     * @param string $name Nombre del usuario
      * @param string $psw Contraseña del usuario
      * @param boolean $save Si esta marcada guardar la sesion
      * @return boolean Devuelve true si se ha registrado el logueo
      */
-    protected function logIn ($nombre, $psw, $save){
+    private function logIn ($name, $psw, $save){
 
-        $resultado = consultaSQL("SELECT * FROM usuario WHERE usuario = '".$nombre."' ;");
+        $bd = new AccesoDatos();
+        $execute = $bd->ejecutar("SELECT * FROM user WHERE user = '".$name."' ;");
+        
+        if ($execute) {
+            while(($row = $bd->nextRow()) !== false){
+                $result[] = $row;
+            }
+        }
+                
+        if(count($result) == 1){
 
-        if(count($resultado) == 1){
+            if(password_verify($psw, $result[0]['password'])){
 
-            if(password_verify($psw, $resultado[0]['psw'])){
-
-                $_SESSION['Nombre'] = $resultado[0]['nombre'];
-                $_SESSION['Usuario'] = $resultado[0]['usuario'];
-                $_SESSION['IDUsuario'] = $resultado[0]['IDUsuario'];
-                $_SESSION['Key'] = session_id();
-                $_SESSION['DireccionIP'] = userDireccionIP();
-                $_SESSION['UltimaActividad'] = $_SERVER['REQUEST_TIME'];
-                $_SESSION['Login'] = true;
-                $_SESSION['Rol'] = $resultado[0]['rol']; 
+                $_SESSION['name'] = $result[0]['name'];
+                $_SESSION['user'] = $result[0]['user'];
+                $_SESSION['user_id'] = $result[0]['user_id'];
+                $_SESSION['key'] = session_id();
+                $_SESSION['ip'] = $this->clientIp();
+                $_SESSION['last_active'] = $_SERVER['REQUEST_TIME'];
+                $_SESSION['login'] = true;
 
                 if($save){
-
-                    setcookie("IDUsuario", $_SESSION['IDUsuario'] , time()+(TIEMPO_LOGIN));
-                    setcookie("Key", $_SESSION['Key'], time()+(TIEMPO_LOGIN));
-                    setcookie("Login", $_SESSION['Login'], time()+(TIEMPO_LOGIN));
-                    setcookie("DireccionIP", $_SESSION['DireccionIP'], time()+(TIEMPO_LOGIN));
-                    setcookie("Rol", $_SESSION['Rol'], time()+(TIEMPO_LOGIN));
-                    setcookie("Usuario", $_SESSION['Usuario'], time()+(TIEMPO_LOGIN));
-                    setcookie("Nombre", $_SESSION['Nombre'], time()+(TIEMPO_LOGIN));
-
-                } // if($save)
+                    setcookie("name", $_SESSION['name'], time()+(TIEMPO_LOGIN));
+                    setcookie("user", $_SESSION['user'], time()+(TIEMPO_LOGIN));
+                    setcookie("user_id", $_SESSION['user_id'] , time()+(TIEMPO_LOGIN));
+                    setcookie("key", $_SESSION['key'], time()+(TIEMPO_LOGIN));
+                    setcookie("ip", $_SESSION['ip'], time()+(TIEMPO_LOGIN));
+                    setcookie("login", $_SESSION['login'], time()+(TIEMPO_LOGIN));
+                } 
                 
                 // Si no deja varias sesiones a la vez por usuario borra las anteriores
-                if(!VARIAS_SESIONES){
-                    borrarSQL("DELETE FROM `sesion` WHERE `idusuario` = '".$_SESSION['IDUsuario']."';");
+                if(!MULTIPLE_SESSIONS){
+                    $bd->ejecutar("DELETE FROM `session` WHERE `user_id` = '".$_SESSION['user_id']."';");
                 }
 
-                insertarSQL("INSERT INTO `sesion` (`idusuario`,`key`,`activodesde`,`ultimaactividad`,`ip`) VALUES (".$_SESSION['IDUsuario'].",'".$_SESSION['Key']."',".$_SESSION['ActivoDesde'].",".$_SESSION['UltimaActividad'].",'".$_SESSION['DireccionIP']."');");
+                $bd->ejecutar("INSERT INTO `session` (`user_id`,`key`,`last_active`,`ip`) VALUES (".$_SESSION['user_id'].",'".$_SESSION['key']."',".$_SESSION['last_active'].",'".$_SESSION['ip']."');");
+                $bd->cerrar();
+                unset($bd); 
                 return true;
                 
-            } // if(password_verify($psw, $resultado[0]['psw']))
+            } 
 
-        } // if(count($resultado) == 1)
+        } 
 
+        $bd->cerrar();
+        unset($bd);
         return false;
         
-    } // function logIn
+    } 
 
     /**
      * Borra los datos de logueo
-     *
-     * @access protected
+     * 
      */
-    protected function logOut (){
+    private function logOut (){
         
-        if(isset($_SESSION['Key'])){
-            borrarSQL("DELETE FROM `sesion` WHERE `key` = '".$_SESSION['Key']."';");
+        unset($_POST);
+        unset($_GET);
+            
+        if(isset($_SESSION['key'])){
+            $bd = new AccesoDatos();
+            $bd->ejecutar("DELETE FROM `sesion` WHERE `key` = '".$_SESSION['key']."';");
+            $bd->cerrar();
+            unset($bd); 
         }
 
         session_unset();
         session_destroy();
         session_start();
         session_regenerate_id(true);
-        $_SESSION['Login'] = false;
+        $_SESSION['login'] = false;
 
-    } // function logOut
+    }
 
     /**
      * Mira la ip desde donde se accede a la web
      *
-     * @access protected
      * @return string IP del cliente
      */
-    protected function ipCliente (){
+    private function clientIp (){
 
         if (!empty($_SERVER['HTTP_CLIENT_IP'])){
             return $_SERVER['HTTP_CLIENT_IP'];
@@ -314,8 +326,6 @@ class Login {
 
         return $_SERVER['REMOTE_ADDR'];
 
-    } // function ipCliente()
+    } 
 
-} // class Seguridad
-
-?>
+} 
